6bda0782e264babe384ff4e31c1a00a1dd491e35
rayen <snrayen8@gmail.com>
2026-01-31 16:34:28 +0100

    modify the db url in alembic env file

--- alembic/env.py
+++ alembic/env.py
@@ -12,9 +12,9 @@ from app.config import settings
 # access to the values within the .ini file in use.
 config = context.config
 config.set_main_option(
-    'sqlalchemy.url', f"postgresql+psycopg2://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
+    'sqlalchemy.url', f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
 )
-
+# postgresql+psycopg2://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 fileConfig(config.config_file_name)
